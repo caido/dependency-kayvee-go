72f1428a52b38cdc89d10baa357c7e69fc6e676a
Emile Fugulin <code@efugulin.com>
2024-09-26 18:57:21 -0400

    Allow handler to be silent

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -66,6 +66,10 @@ func (l *logHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		"canary":           l.isCanary,
 	})
 
+	if silent, ok := data["silent"].(bool); ok && silent {
+		return
+	}
+
 	// check if the user has opted in to rolling up middleware logs
 	if globalRollupRouter != nil && globalRollupRouter.ShouldRollup(data) {
 		globalRollupRouter.Process(data)
